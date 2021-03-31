04df01b52e09ea19a4817ee8c505ea33ed668cdc
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-01 01:55:29 +0530

    Add project files.

---
+++ ToDo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void TextChangeHandler(object sender, TextChangedEventArgs args)
+        {
+            Console.WriteLine(args.NewTextValue);
+        }
+
+        private void EntryCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Enter pressed");
+            Label newTodo = new Label();
+            newTodo.Text = InputField.Text;
+            Console.WriteLine(InputField.Text);
+            ToDoList.Children.Add(newTodo);
+            InputField.Text = "";
+        }
+    }
+}
